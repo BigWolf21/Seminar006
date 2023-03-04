26f8ff1a3cd62ffaf732a2ec49d504784f5eda0d
Your Name~git <you@example.com>
2023-03-04 19:17:01 +0700

    Решение задач семинара №6

---
+++ HW01/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int GetNumber(string text)
+{
+  int value = 0;
+  bool flag = false;
+  while (!flag)
+  {
+    Console.Write(text);
+    flag = int.TryParse(Console.ReadLine(), out value);
+  }
+  return value;
+}
+
+int[] CreateArray(int size)
+{
+  return new int[size];
+}
+
+void FillByUser(int[] array)
+{
+  int size = array.Length;
+  for (int i = 0; i < size; i++)
+  {
+    array[i] = GetNumber($"Введите {i + 1} элемент: - ");
+  }
+}
+
+string Print(int[] array)
+{
+  string result = String.Empty;
+  int len = array.Length;
+  for (int i = 0; i < len; i++)
+  {
+    result += array[i] + ", ";
+  }
+  return result;
+}
+
+int Account(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i] > 0)
+    {
+      count += +1;
+    }
+  }
+  return count;
+}
+
+int value = GetNumber("Какое множество чисел будет введенно - ");
+int[] array = CreateArray(value);
+FillByUser(array);
+Console.WriteLine($"[{Print(array)}]");
+Console.WriteLine($"Количество чисел больше '0' = {Account(array)}");
---
+++ HW02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double GetDoubleNumber(string text)
+{
+  double value = 0;
+  bool flag = false;
+  while (!flag)
+  {
+    Console.Write(text);
+    flag = double.TryParse(Console.ReadLine(), out value);
+  }
+  return value;
+}
+
+double b1 = GetDoubleNumber("Введите b1: ");
+double k1 = GetDoubleNumber("Введите k1: ");
+double b2 = GetDoubleNumber("Введите b2: ");
+double k2 = GetDoubleNumber("Введите k2: ");
+ 
+ 
+double x = Math.Round(-(b1 - b2) / (k1 - k2), 3);
+double y = Math.Round(k1 * x + b1, 3);
+
+ 
+Console.WriteLine($"Пересечение в точке: ({x};{y})");
